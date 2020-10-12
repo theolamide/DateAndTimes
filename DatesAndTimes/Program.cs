f4a8449e3bb69ed5f98f5db354175acc68a93708
theolamide <ajadidan@gmail.com>
2020-10-11 23:25:11 -0400

    Add project files.

---
+++ DatesAndTimes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DatesAndTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime  myValue = DateTime.Now;
+            Console.WriteLine(myValue.ToString());
+            Console.WriteLine(myValue.ToShortDateString());
+            Console.WriteLine(myValue.ToShortTimeString());
+            Console.WriteLine(myValue.ToLongDateString());
+            Console.WriteLine(myValue.ToLongTimeString());
+
+            Console.WriteLine(myValue.AddDays(3).ToLongDateString());
+
+            Console.ReadLine();
+
+        }
+    }
+}
