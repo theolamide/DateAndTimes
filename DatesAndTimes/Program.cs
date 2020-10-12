37fb621a3dd052c2c5e7d6dbdd72198d06753daa
theolamide <ajadidan@gmail.com>
2020-10-11 23:33:25 -0400

    Dates and Time Complete

--- DatesAndTimes/Program.cs
+++ DatesAndTimes/Program.cs
@@ -15,8 +15,20 @@ namespace DatesAndTimes
 
             Console.WriteLine(myValue.AddDays(3).ToLongDateString());
 
-            Console.ReadLine();
+            Console.WriteLine(myValue.AddHours(3).ToLongTimeString());
 
+            Console.WriteLine(myValue.AddDays(-3).ToLongDateString()); //Past 3 days
+
+            Console.WriteLine(myValue.Month);
+
+            //DateTime myBirthday = new DateTime(1969, 12, 7);
+            //Console.WriteLine(myBirthday.ToShortDateString());
+
+            DateTime myBirthday = DateTime.Parse("04/02/1991");
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+            Console.Write(myAge.TotalDays);
+            Console.Write(" Days");
+            Console.ReadLine();
         }
     }
 }
